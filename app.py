a5e4018dcc3b9b49f8100dc8539d305d7ffcc112
Dj-cmd-ai <dhananjay@gatidriver.com>
2025-12-24 22:53:35 +0530

    Changes in index and app for profile image

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.png')
+    image_file = db.Column(db.String(255), nullable=False, default='https://ui-avatars.com/api/?name=User&background=random')
     # Update this from 128 to 255
     password_hash = db.Column(db.String(255), nullable=False) 
     posts = db.relationship('Post', backref='author', lazy=True)
